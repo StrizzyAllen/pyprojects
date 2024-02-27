1a3ed681e15ad2647299a608d8fcaa5c303a465c
Isaiah Strouse <isaiahstrouse66@gmail.com>
2024-02-27 13:18:37 -0600

    debugged and tested. Program run succesful

--- bagels.py
+++ bagels.py
@@ -1,89 +1,98 @@
-# Bagels is a deductive logic game where you must guess the secret number 
-# strizzyallen @ github.com/strizzyallen
+## Bagles Logic Game by github.com/strizzyallen inspired by  Al Sweigarts book 'The Big Book of Small Python Projects'.
+## Bagels is a deductive logic game where the user must guess the secret 3 digit number.
+## The user will get 10 chances to guess correctly. There will also be hints given, for example:
+
+## When I say:      What it means:
+## Pico             One number is correct but in the wrong position
+## Fermi            One number is correct and in the right position
+## Bagels           No number is correct
 
 import random
 
-NUM_DIGITS = 3 
-MAX_GUESSES = 10 
+NUM_DIGITS = 3
+MAX_GUESSES = 10
 
 
 def main():
-    print('''Bagels, a deductive logic game. 
-          
-     I am thinking of a {}-digit number with no repeated digits.
-     Try to guess what it is. Here are some hints:
-     When I say:   That means:
-     Pico          One digit is correct but in the wrong place.
-     Fermi         One digit is correct and in the right place.
-     Bagels        No digit is correct.
+    print(
+        """Bagels, a deductive logic game.
 
-     Example, if the secret number is 248 and your guess was 843, the hint would be fermi pico'''.format(NUM_DIGITS))
-    
-    while True: # main game loop
-        # This stores the secret number the player needs to guess:
+    I am thinking of a secret {}-digit number with no repeated digits. Try to guess what it is. Here are some clues:
+
+    When I say:      What it means:
+    Pico             One number is correct but in the wrong position
+    Fermi            One number is correct and in the right position
+    Bagels           No number is correct
+
+    For example, if the secret number is 248 and your guess is 843, the clues would be Fermi Pico.""".format(
+            NUM_DIGITS
+        )
+    )
+
+    while True:  # main game loop
+        # this stores the secret number
         secretNum = getSecretNum()
-        print('I have thought of a secret number.')
-        print(' You have {} guesses to get it.'.format(MAX_GUESSES))
+        print("I have thought up a number")
+        print(" You have {} guesses to get it.".format(MAX_GUESSES))
 
-        numGuesses = 1 
+        numGuesses = 1
         while numGuesses <= MAX_GUESSES:
-            guess = ''
-            # keep looping until valid guess is entered
+            guess = ""
+            # keep looping until they enter a valid guess:
             while len(guess) != NUM_DIGITS or not guess.isdecimal():
-                print('Guess #{}: '.format(numGuesses))
-                guess = input('> ')
-            
+                print("Guess #{}: ".format(numGuesses))
+                guess = input("> ")
             clues = getClues(guess, secretNum)
             print(clues)
             numGuesses += 1
 
-            if guess == secretNum: 
-                break # correct guess, so break out of this loop:
+            if guess == secretNum:
+                break  # they got it correct, break out of the loop
             if numGuesses > MAX_GUESSES:
-                print('You ran out of guesses.')
-                print('The answer was {}.'.format(secretNum))
-
+                print("You ran out of guesses")
+                print("The answer was {}.".format(secretNum))
         # Ask player if they want to play again
-        print('Do you want to play again? (yes or no)')
-        if not input('> ').lower().startswith('y'):
+        print("Do you want to play again? (yes or no)")
+        if not input("> ").lower().startswith("y"):
             break
-        print('Thanks for playing!')
+    print("Thanks for playing!")
+
 
 def getSecretNum():
     """Returns a string made up of NUM_DIGITS unique random digits."""
-    numbers = list('0123456789') # Create a list of digits 0 to 9.
-    random.shuffle(numbers) # shuffle them into random order.
+    numbers = list("0123456789")  # list of digits
+    random.shuffle(numbers)  # shuffle into random order
 
     # Get the first NUM_DIGITS digits in the list for the secret number:
-    secretNum = ''
+    secretNum = ""
     for i in range(NUM_DIGITS):
         secretNum += str(numbers[i])
-        return secretNum
-    
+    return secretNum
+
+
 def getClues(guess, secretNum):
-    """Returns a string with the pico, fermi bagels clues for the guess and secret number pair."""
+    """Retruns a string with the hints"""
     if guess == secretNum:
-        return 'You got it!!'
-    
+        return "You got it, good Job!"
     clues = []
 
     for i in range(len(guess)):
         if guess[i] == secretNum[i]:
-            # A correct digit is in the correct place.
-            clues.append('Fermi')
+            # correct digit in the correct place
+            clues.append("Fermi")
         elif guess[i] in secretNum:
-            # A correct digit is in the wrong place.
-            clues.append('Pico')
-            if len(clues) == 0:
-                return 'Bagels' # There are no correct digits
-            else:
-                # sort the clues into alphabetical order so their original order doesnt give information away
-                clues.short()
-                # make a single string form the list of string clues
-                return ' '.join(clues)
-            
-# if the program is run (instead of imported), run the game:
-if __name__ == '__main__':
-    main()
+            # correct digit is in the wrong place
+            clues.append("Pico")
+        if len(clues) == 0:
+            return "Bagels"  # no correct digits
+        else:
+            # sort the clues into alphabetical order so their original
+            # order doesn't give information away
+            clues.sort()
+            # make a single string from the list of string clues
+            return " ".join(clues)
 
-    
+
+# if the program is run (instead of imported), run the game:
+if __name__ == "__main__":
+    main()
