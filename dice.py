a17b8a9649e2b6a6e11b0b2e7340bfb46ece1c16
Isaiah Strouse <isaiahstrouse66@gmail.com>
2024-02-29 19:19:35 -0600

    Created ASCII diagram for dices

--- dice.py
+++ dice.py
@@ -49,6 +49,41 @@ DIE_HEIGHT = len(DICE_ART[1])
 DIE_WIDTH = len(DICE_ART[1][0])
 DIE_FACE_SEPARATOR = " "
 
+def generate_dice_faces_diagram(dice_values):
+    """Return an ASCII diagram of dices from 'dice_values'.
+    
+    The string returned contains an ASCII representation of each die.
+    For example, if 'dice_values = [4, 1, 3, 2]' then the string
+    returned looks like this:
+    
+     ~~~~~~~~~~~~~~~~~~~ RESULTS ~~~~~~~~~~~~~~~~~~~
+    ┌─────────┐ ┌─────────┐ ┌─────────┐ ┌─────────┐
+    │  ●   ●  │ │         │ │  ●      │ │  ●      │
+    │         │ │    ●    │ │    ●    │ │         │
+    │  ●   ●  │ │         │ │      ●  │ │      ●  │
+    └─────────┘ └─────────┘ └─────────┘ └─────────┘
+    """
+    # Generate a list of dice faces from DICE_ART
+    dice_faces = []
+    for value in dice_values:
+        dice_faces.append(DICE_ART[value])
+
+    # Generate a list containing the dice faces rows
+    dice_faces_rows = []
+    for row_idx in range(DIE_HEIGHT):
+        row_components = []
+        for die in dice_faces:
+            row_components.append(die[row_idx])
+        row_string = DIE_FACE_SEPARATOR.join(row_components)
+        dice_faces_rows.append(row_string)
+
+    # Generate header with the word "RESULTS" centered
+    width = len(dice_faces_rows[0])
+    diagram_header = " RESULTS ".center(width, "~")
+
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram
+
 
 def roll_dice(num_dice):
     """Return a list of integers with length 'num_dice',
@@ -83,3 +118,7 @@ num_dice_input = input("How many dice do you want to roll? [1-6]" )
 num_dice = parse_input(num_dice_input)
 # 2. Roll the dice
 roll_results = roll_dice(num_dice)
+# 3. Generate the ASCII diagram of dice faces
+dice_faces_diagram = generate_dice_faces_diagram(roll_results)
+# 4. Display the diagram
+print(f"\n{dice_faces_diagram}")
